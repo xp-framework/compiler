35cdf07bdec5542009008588c64e23ec57faac08
Timm Friebe <thekid@thekid.de>
2017-10-14 20:55:18 +0200

    Add links to RFCs definining PHP 5.6 features

--- src/main/php/lang/ast/emit/PHP56.class.php
+++ src/main/php/lang/ast/emit/PHP56.class.php
@@ -1,9 +1,24 @@
 <?php namespace lang\ast\emit;
 
+/**
+ * PHP 5.6 syntax
+ *
+ * @see  https://wiki.php.net/rfc/pow-operator
+ * @see  https://wiki.php.net/rfc/variadics
+ * @see  https://wiki.php.net/rfc/argument_unpacking
+ * @see  https://wiki.php.net/rfc/use_function - Not yet implemented
+ */
 class PHP56 extends \lang\ast\Emitter {
 
   protected function type($name) {
-    static $unsupported= ['void' => 71, 'iterable' => 71];
+    static $unsupported= [
+      'void'     => 71,
+      'iterable' => 71,
+      'string'   => 70,
+      'int'      => 70,
+      'bool'     => 70,
+      'float'    => 70
+    ];
 
     return isset($unsupported[$name]) ? null : $name;
   }
