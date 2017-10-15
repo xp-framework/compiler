adb313c60157a0f373517edbf45a475d20aa1a7f
Timm Friebe <thekid@thekid.de>
2017-10-15 16:22:47 +0200

    Add tests for braces in various situations

---
+++ src/test/php/lang/ast/unittest/emit/BracesTest.class.php
@@ -0,0 +1,45 @@
+<?php namespace lang\ast\unittest\emit;
+
+class BracesTest extends EmittingTest {
+
+  #[@test]
+  public function inc() {
+    $r= $this->run('class <T> {
+      private $id= 0;
+
+      public function run() {
+        return "test".(++$this->id);
+      }
+    }');
+
+    $this->assertEquals('test1', $r);
+  }
+
+  #[@test]
+  public function no_braces_necessary_around_new() {
+    $r= $this->run('class <T> {
+      public function run() {
+        return new \\util\\Date(250905600)->getTime();
+      }
+    }');
+
+    $this->assertEquals(250905600, $r);
+  }
+
+  #[@test]
+  public function property_vs_method_ambiguity() {
+    $r= $this->run('class <T> {
+      private $f;
+
+      public function __construct() {
+        $this->f= function($arg) { return $arg; };
+      }
+
+      public function run() {
+        return ($this->f)("test");
+      }
+    }');
+
+    $this->assertEquals('test', $r);
+  }
+}
