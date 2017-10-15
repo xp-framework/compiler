703f0fbfa95d7cf0fa7ad50a3fb9f902c5c74f9b
Timm Friebe <thekid@thekid.de>
2017-10-15 14:40:29 +0200

    Add test for multiple catch statement

---
+++ src/test/php/lang/ast/unittest/emit/MultipleCatchTest.class.php
@@ -0,0 +1,30 @@
+<?php namespace lang\ast\unittest\emit;
+
+use lang\IllegalArgumentException;
+use lang\IllegalStateException;
+
+/**
+ * Multiple catch
+ *
+ * @see  https://wiki.php.net/rfc/multiple-catch
+ */
+class MultipleCatchTest extends EmittingTest {
+
+  #[@test, @values([
+  #  IllegalArgumentException::class,
+  #  IllegalStateException::class
+  #])]
+  public function catch_both($type) {
+    $t= $this->type('class <T> {
+      public function run($t) {
+        try {
+          throw $t;
+        } catch (\\lang\\IllegalArgumentException | \\lang\\IllegalStateException $e) {
+          return "caught ".get_class($e);
+        }
+      }
+    }');
+
+    $this->assertEquals('caught '.$type, $t->newInstance()->run(new $type('test')));
+  }
+}
