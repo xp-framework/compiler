93b33ae294dd8d9643e3a957a4dd79d92c53a509
Timm Friebe <thekid@thekid.de>
2017-10-21 16:56:54 +0200

    Add tests for issue #7

---
+++ src/test/php/lang/ast/unittest/emit/TernaryTest.class.php
@@ -0,0 +1,28 @@
+<?php namespace lang\ast\unittest\emit;
+
+class TernaryTest extends EmittingTest {
+
+  #[@test, @values(map= [true => "OK", false => "Fail"])]
+  public function ternary($value, $result) {
+    $this->assertEquals($result, $this->run(
+      'class <T> {
+        public function run($value) {
+          return $value ? "OK" : "Fail";
+        }
+      }',
+      $value
+    ));
+  }
+
+  #[@test, @values(map= ["OK" => "OK", null => "Fail"])]
+  public function short_ternary($value, $result) {
+    $this->assertEquals($result, $this->run(
+      'class <T> {
+        public function run($value) {
+          return $value ?: "Fail";
+        }
+      }',
+      $value
+    ));
+  }
+}
