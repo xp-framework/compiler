e81ca602683ec0aa4918de9539ffb477898ac485
Timm Friebe <thekid@thekid.de>
2017-11-04 16:56:38 +0100

    Add test for comments

---
+++ src/test/php/lang/ast/unittest/emit/CommentsTest.class.php
@@ -0,0 +1,35 @@
+<?php namespace lang\ast\unittest\emit;
+
+class CommentsTest extends EmittingTest {
+
+  #[@test]
+  public function on_class() {
+    $t= $this->type('/** Test */ class <T> { }');
+    $this->assertEquals('Test', $t->getComment());
+  }
+
+  #[@test]
+  public function on_interface() {
+    $t= $this->type('/** Test */ interface <T> { }');
+    $this->assertEquals('Test', $t->getComment());
+  }
+
+  #[@test]
+  public function on_trait() {
+    $t= $this->type('/** Test */ trait <T> { }');
+    $this->assertEquals('Test', $t->getComment());
+  }
+
+  #[@test]
+  public function on_method() {
+    $t= $this->type('class <T> {
+
+      /** Test */
+      public function fixture() {
+        // NOOP
+      }
+    }');
+
+    $this->assertEquals('Test', $t->getMethod('fixture')->getComment());
+  }
+}
