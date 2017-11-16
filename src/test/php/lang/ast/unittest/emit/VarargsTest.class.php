6d199aa737ba8e31dc860e69da0f7247896b3081
Timm Friebe <thekid@thekid.de>
2017-11-16 22:24:50 +0100

    Add tests for varargs

---
+++ src/test/php/lang/ast/unittest/emit/VarargsTest.class.php
@@ -0,0 +1,30 @@
+<?php namespace lang\ast\unittest\emit;
+
+class VarargsTest extends EmittingTest {
+
+  #[@test]
+  public function vsprintf() {
+    $r= $this->run('class <T> {
+      private function format(string $format, ... $args) ==> vsprintf($format, $args);
+
+      public function run() {
+        return $this->format("Hello %s", "Test");
+      }
+    }');
+
+    $this->assertEquals('Hello Test', $r);
+  }
+
+  #[@test]
+  public function list_of() {
+    $r= $this->run('class <T> {
+      private function listOf(string... $args) ==> $args;
+
+      public function run() {
+        return $this->listOf("Hello", "Test");
+      }
+    }');
+
+    $this->assertEquals(['Hello', 'Test'], $r);
+  }
+}
