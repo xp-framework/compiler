e07522b8c277c0729e858367beeab01c73fc5299
Timm Friebe <thekid@thekid.de>
2018-06-24 18:24:18 +0200

    Refactor test to work completely without external resources

--- src/test/php/lang/ast/unittest/loader/CompilingClassLoaderTest.class.php
+++ src/test/php/lang/ast/unittest/loader/CompilingClassLoaderTest.class.php
@@ -1,18 +1,39 @@
 <?php namespace lang\ast\unittest\loader;
 
-use unittest\TestCase;
-use lang\ast\CompilingClassLoader;
-use lang\ClassFormatException;
-use lang\ClassLoader;
 use io\File;
 use io\FileUtil;
+use lang\ClassFormatException;
+use lang\ClassLoader;
 use lang\Environment;
+use lang\ast\CompilingClassLoader;
+use unittest\TestCase;
 
 class CompilingClassLoaderTest extends TestCase {
   private static $runtime;
 
   static function __static() {
     self::$runtime= defined('HHVM_VERSION') ? 'HHVM.'.HHVM_VERSION : 'PHP.'.PHP_VERSION;
+  }
+
+  /**
+   * Loads a class from source
+   *
+   * @param  string $type
+   * @param  string $source
+   * @return lang.XPClass
+   */
+  private function load($type, $source) {
+    $f= new File(Environment::tempDir(), $type.'.php');
+    FileUtil::setContents($f, sprintf($source, 'ns'.uniqid()));
+    $cl= ClassLoader::registerPath($f->getPath());
+
+    $loader= new CompilingClassLoader(self::$runtime);
+    try {
+      return $loader->loadClass($type);
+    } finally {
+      ClassLoader::removeLoader($cl);
+      $f->unlink();
+    }
   }
 
   #[@test]
@@ -22,25 +43,14 @@ class CompilingClassLoaderTest extends TestCase {
 
   #[@test]
   public function load_class() {
-    $loader= new CompilingClassLoader(self::$runtime);
-    $this->assertEquals('Tests', $loader->loadClass('lang.ast.unittest.loader.Tests')->getSimpleName());
+    $this->assertEquals('Tests', $this->load('Tests', '<?php namespace %s; class Tests { }')->getSimpleName());
   }
 
-  #[@test]
+  #[@test, @expect(
+  #  class= ClassFormatException::class,
+  #  withMessage= 'Syntax error in Errors.php, line 2: Expected ";", have "Syntax"'
+  #)]
   public function load_class_with_syntax_errors() {
-    $f= new File(Environment::tempDir(), 'Errors.php');
-    FileUtil::setContents($f, "<?php\n<Syntax error in line 2>");
-    $cl= ClassLoader::registerPath($f->getPath());
-
-    $loader= new CompilingClassLoader(self::$runtime);
-    try {
-      $loader->loadClass('Errors');
-      $this->fail('No exception raised', null, ClassFormatException::class);
-    } catch (ClassFormatException $expected) {
-      $this->assertEquals('Syntax error in Errors.php, line 2: Expected ";", have "Syntax"', $expected->getMessage());
-    } finally {
-      ClassLoader::removeLoader($cl);
-      $f->unlink();
-    }
+    $this->load('Errors', "<?php\n<Syntax error in line 2>");
   }
 }
