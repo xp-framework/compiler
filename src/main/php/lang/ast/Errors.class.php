415fe7fa6d21f4dad73cf469ef44737adcbe8c74
Timm Friebe <thekid@thekid.de>
2018-12-29 15:41:41 +0100

    QA: Apidocs

--- src/main/php/lang/ast/Errors.class.php
+++ src/main/php/lang/ast/Errors.class.php
@@ -8,7 +8,7 @@ class Errors extends IllegalStateException {
   /**
    * Creates a new error
    *
-   * @param  lang.ast.Error $errors
+   * @param  lang.ast.Error[] $errors
    * @param  string $file
    */
   public function __construct($errors, $file) {
@@ -27,11 +27,18 @@ class Errors extends IllegalStateException {
         }
         $message.= '}';
     }
+
     parent::__construct($message);
     $this->file= $file;
     $this->line= $errors[0]->getLine();
   }
 
+  /**
+   * Returns diagnostics
+   *
+   * @param  string $indent
+   * @return string
+   */
   public function diagnostics($indent= '') {
     return str_replace("\n", "\n".$indent, $this->message);
   }
