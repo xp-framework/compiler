6ccaf8943f1fe14ff0d122390a16bc2ad2ca1e6c
Timm Friebe <thekid@thekid.de>
2019-06-14 19:22:59 +0200

    Prevent "callable" and "void" types from being used on properties

--- src/main/php/lang/ast/emit/PHP74.class.php
+++ src/main/php/lang/ast/emit/PHP74.class.php
@@ -1,6 +1,11 @@
 <?php namespace lang\ast\emit;
 
+use lang\ast\ArrayType;
 use lang\ast\Emitter;
+use lang\ast\FunctionType;
+use lang\ast\MapType;
+use lang\ast\Type;
+use lang\ast\UnionType;
 
 /**
  * PHP 7.4 syntax
@@ -21,7 +26,18 @@ class PHP74 extends Emitter {
       DETAIL_ARGUMENTS   => []
     ];
 
-    $this->out->write(implode(' ', $property->modifiers).' '.$this->type($property->type).' $'.$property->name);
+    // See https://wiki.php.net/rfc/typed_properties_v2#supported_types
+    if (null === $property->type || $property->type instanceof UnionType || $property->type instanceof FunctionType) {
+      $hint= '';
+    } else if ($property->type instanceof ArrayType || $property->type instanceof MapType) {
+      $hint= 'array';
+    } else if ($property->type instanceof Type && 'callable' !== $property->type->literal() && 'void' !== $property->type->literal()) {
+      $hint= $property->type->literal();
+    } else {
+      $hint= '';
+    }
+
+    $this->out->write(implode(' ', $property->modifiers).' '.$hint.' $'.$property->name);
     if (isset($property->expression)) {
       $this->out->write('=');
       $this->emit($property->expression);
