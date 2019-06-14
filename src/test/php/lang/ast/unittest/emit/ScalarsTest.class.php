8b2bd3b8d2ff1c92e4bd1b20d6de908af773c5d0
Timm Friebe <thekid@thekid.de>
2019-06-14 20:51:28 +0200

    Add test for scalars

---
+++ src/test/php/lang/ast/unittest/emit/ScalarsTest.class.php
@@ -0,0 +1,34 @@
+<?php namespace lang\ast\unittest\emit;
+
+class ScalarsTest extends EmittingTest {
+
+  #[@test, @values([
+  #  ['0', 0],
+  #  ['1', 1],
+  #  ['-1', -1],
+  #  ['0xff', 255],
+  #  ['0755', 493],
+  #  ['1.5', 1.5],
+  #  ['-1.5', -1.5],
+  #])]
+  public function numbers($literal, $result) {
+    $this->assertEquals($result, $this->run('class <T> { public function run() { return '.$literal.'; } }'));
+  }
+
+  #[@test, @values([
+  #  ['""', ''],
+  #  ['"Test"', 'Test'],
+  #])]
+  public function strings($literal, $result) {
+    $this->assertEquals($result, $this->run('class <T> { public function run() { return '.$literal.'; } }'));
+  }
+
+  #[@test, @values([
+  #  ['true', true],
+  #  ['false', false],
+  #  ['null', null],
+  #])]
+  public function constants($literal, $result) {
+    $this->assertEquals($result, $this->run('class <T> { public function run() { return '.$literal.'; } }'));
+  }
+}
