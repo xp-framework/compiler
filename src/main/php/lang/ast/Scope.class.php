09947daa34eaca227a808b6016357ef07c7c767a
Timm Friebe <thekid@thekid.de>
2019-08-10 16:31:50 +0200

    Handle case when an empty name is passed to resolve()

--- src/main/php/lang/ast/Scope.class.php
+++ src/main/php/lang/ast/Scope.class.php
@@ -60,7 +60,9 @@ class Scope {
    * @return string
    */
   public function resolve($name) {
-    if (isset(self::$reserved[$name])) {
+    if (null === $name) {
+      return '';
+    } else if (isset(self::$reserved[$name])) {
       return $name;
     } else if ('\\' === $name[0]) {
       return $name;
