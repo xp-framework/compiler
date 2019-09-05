f8d29f599950d15176d537b654714ba60f320561
Timm Friebe <thekid@thekid.de>
2019-09-05 22:33:59 +0200

    QA: API docs

--- src/main/php/lang/ast/Symbol.class.php
+++ src/main/php/lang/ast/Symbol.class.php
@@ -1,21 +1,27 @@
 <?php namespace lang\ast;
 
-class Symbol implements \lang\Value {
+use lang\Value;
+
+class Symbol implements Value {
   public $id, $lbp= 0, $std= null, $nud, $led;
 
-  public function error($message) {
-    throw new Error($message.' '.$this->id);
-  }
-
+  /** @return string */
   public function hashCode() {
     return $this->id;
   }
 
+  /** @return string */
   public function toString() {
     return nameof($this).'("'.$this->id.'", lbp= '.$this->lbp.')';
   }
 
-  public function compareTo($that) {
-    return $that instanceof self ? strcmp($this->id, $that->id) : 1;
+  /**
+   * Compare
+   *
+   * @param  var $value
+   * @return int
+   */
+  public function compareTo($value) {
+    return $value instanceof self ? strcmp($this->id, $value->id) : 1;
   }
 }
