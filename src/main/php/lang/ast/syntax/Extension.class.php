760e8c53fca2bc36cfaa06839940b2e580e82db9
Timm Friebe <thekid@thekid.de>
2019-09-06 14:34:42 +0200

    QA: API documentation

--- src/main/php/lang/ast/syntax/Extension.class.php
+++ src/main/php/lang/ast/syntax/Extension.class.php
@@ -2,5 +2,12 @@
 
 interface Extension {
 
+  /**
+   * Register this syntax extension with the language and emitter
+   *
+   * @param  lang.ast.Language $language
+   * @param  lang.ast.Emitter $emitter
+   * @return void
+   */
   public function setup($language, $emitter);
 }
