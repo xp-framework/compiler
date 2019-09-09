10a664d8f83aa3220ab4ce0eb4204a97bbcf817e
Timm Friebe <thekid@thekid.de>
2019-09-09 13:38:23 +0200

    Extend PHP base class

--- src/main/php/lang/ast/emit/HHVM320.class.php
+++ src/main/php/lang/ast/emit/HHVM320.class.php
@@ -1,12 +1,10 @@
 <?php namespace lang\ast\emit;
 
-use lang\ast\Emitter;
-
 /**
  * HHVM syntax. Like PHP 7.0, but does not allow variadic parameters with
  * types.
  */
-class HHVM320 extends Emitter {
+class HHVM320 extends PHP {
   use OmitPropertyTypes, OmitConstModifiers;
   use RewriteNullCoalesceAssignment, RewriteLambdaExpressions, RewriteMultiCatch;
 
