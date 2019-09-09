65b58a0c8012d46067731fd795a00286bcd56cce
Timm Friebe <thekid@thekid.de>
2019-09-09 16:10:15 +0200

    QA: Document throw expressions

--- src/main/php/lang/ast/syntax/php/ThrowExpressions.class.php
+++ src/main/php/lang/ast/syntax/php/ThrowExpressions.class.php
@@ -3,6 +3,19 @@
 use lang\ast\nodes\ThrowExpression;
 use lang\ast\syntax\Extension;
 
+/**
+ * Throw expressions
+ *
+ * ```php
+ * // Syntax
+ * return $user ?? throw new IllegalStateException('No such user');
+ *
+ * // Rewritten to
+ * return $user ?? (function() { throw ... })();
+ * ```
+ * @see  https://github.com/xp-framework/compiler/pull/53
+ * @test xp://lang.ast.unittest.emit.ExceptionsTest
+ */
 class ThrowExpressions implements Extension {
 
   public function setup($language, $emitter) {
