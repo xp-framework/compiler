f192893a0603909f34a9437e46503346f7a5d317
Timm Friebe <thekid@thekid.de>
2019-10-01 03:01:49 +0200

    Remove superfluous test code

--- src/test/php/lang/ast/unittest/emit/AnonymousClassTest.class.php
+++ src/test/php/lang/ast/unittest/emit/AnonymousClassTest.class.php
@@ -61,51 +61,8 @@ class AnonymousClassTest extends EmittingTest {
           public function fixture() { }
         };
       }
-
-      public function fixture() { }
     }');
 
     $this->assertEquals(['inside' => null], typeof($r)->getMethod('fixture')->getAnnotations());
-  }
-
-  #[@test]
-  public function method_annotations_with_constructor() {
-    $r= $this->run('class <T> {
-      public function run() {
-        return new class("test") {
-          public $name;
-
-          public function __construct($name) {
-            $this->name= $name;
-          }
-
-          <<test>>
-          public function fixture() { }
-        };
-      }
-    }');
-    $this->assertEquals(['test' => null], typeof($r)->getMethod('fixture')->getAnnotations());
-    $this->assertEquals('test', $r->name);
-  }
-
-  #[@test]
-  public function method_annotations_with_inherited_constructor() {
-    $r= $this->run('class <T> {
-      public $name;
-
-      public function __construct($name= null) {
-        $this->name= $name;
-      }
-
-      public function run() {
-        return new class("test") extends <T> {
-
-          <<test>>
-          public function fixture() { }
-        };
-      }
-    }');
-    $this->assertEquals(['test' => null], typeof($r)->getMethod('fixture')->getAnnotations());
-    $this->assertEquals('test', $r->name);
   }
 }
