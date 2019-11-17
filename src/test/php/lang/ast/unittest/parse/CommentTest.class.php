a85a0c06777460f6f88c63becd661c9ebc3290ba
Timm Friebe <thekid@thekid.de>
2019-11-17 14:39:16 +0100

    Test additional variants of comments

--- src/test/php/lang/ast/unittest/parse/CommentTest.class.php
+++ src/test/php/lang/ast/unittest/parse/CommentTest.class.php
@@ -13,6 +13,13 @@ class CommentTest extends ParseTest {
   }
 
   #[@test]
+  public function oneline_double_slash_at_end() {
+    $this->assertParsed([new Literal('"test"', 2)], '
+      "test";  // This is a comment
+    ');
+  }
+
+  #[@test]
   public function two_oneline_double_slash() {
     $this->assertParsed([new Literal('"test"', 4)], '
       // This is a comment
@@ -30,6 +37,13 @@ class CommentTest extends ParseTest {
   }
 
   #[@test]
+  public function oneline_hashtag_at_end() {
+    $this->assertParsed([new Literal('"test"', 2)], '
+      "test";  # This is a comment
+    ');
+  }
+
+  #[@test]
   public function two_oneline_hashtags() {
     $this->assertParsed([new Literal('"test"', 4)], '
       # This is a comment
@@ -43,6 +57,20 @@ class CommentTest extends ParseTest {
     $this->assertParsed([new Literal('"test"', 3)], '
       /* This is a comment */
       "test";
+    ');
+  }
+
+  #[@test]
+  public function oneline_slash_asterisk_at_end() {
+    $this->assertParsed([new Literal('"test"', 2)], '
+      "test";  /* This is a comment */
+    ');
+  }
+
+  #[@test]
+  public function oneline_slash_asterisk_inbetween() {
+    $this->assertParsed([new Literal('"before"', 2), new Literal('"after"', 2)], '
+      "before"; /* This is a comment */ "after";
     ');
   }
 
