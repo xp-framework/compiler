548fe2e2d8f0cbc145e3ccc0664e6522aa43bafd
Timm Friebe <thekid@thekid.de>
2020-02-15 15:01:48 +0100

    Add native support for arbitrary expressions with new

--- src/main/php/lang/ast/emit/PHP80.class.php
+++ src/main/php/lang/ast/emit/PHP80.class.php
@@ -1,5 +1,7 @@
 <?php namespace lang\ast\emit;
 
+use lang\ast\Node;
+
 /**
  * PHP 8.0 syntax
  *
@@ -11,4 +13,17 @@ class PHP80 extends PHP {
   protected $unsupported= [
     'mixed'    => null,
   ];
+
+  protected function emitNew($result, $new) {
+    if ($new->type instanceof Node) {
+      $result->out->write('new (');
+      $this->emitOne($result, $new->type);
+      $result->out->write(')(');
+    } else {
+      $result->out->write('new '.$new->type.'(');
+    }
+
+    $this->emitArguments($result, $new->arguments);
+    $result->out->write(')');
+  }
 }
