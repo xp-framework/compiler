c81843674569bda4b5ca0134ad0147199a9d20aa
Timm Friebe <thekid@thekid.de>
2020-05-02 16:24:18 +0200

    QA: Update link to implemented property types RFC

--- src/test/php/lang/ast/unittest/emit/PropertyTypesTest.class.php
+++ src/test/php/lang/ast/unittest/emit/PropertyTypesTest.class.php
@@ -6,7 +6,7 @@ use unittest\Assert;
  * Property types
  *
  * @see  https://docs.hhvm.com/hack/types/type-system
- * @see  https://wiki.php.net/rfc/property_type_hints (Draft)
+ * @see  https://wiki.php.net/rfc/typed_properties_v2
  */
 class PropertyTypesTest extends EmittingTest {
 
