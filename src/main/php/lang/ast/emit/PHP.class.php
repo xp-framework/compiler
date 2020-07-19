b1ba3a624488491d11b549b3e4d08637761e7939
Timm Friebe <thekid@thekid.de>
2020-07-19 23:36:55 +0200

    Emit null-safe instance operator `?->`

--- src/main/php/lang/ast/emit/PHP.class.php
+++ src/main/php/lang/ast/emit/PHP.class.php
@@ -765,6 +765,21 @@ abstract class PHP extends Emitter {
     }
   }
 
+  protected function emitNullsafeInstance($result, $instance) {
+    $t= $result->temp();
+    $result->out->write('null===('.$t.'=');
+    $this->emitOne($result, $instance->expression);
+    $result->out->write(')?null:'.$t.'->');
+
+    if ('literal' === $instance->member->kind) {
+      $result->out->write($instance->member->expression);
+    } else {
+      $result->out->write('{');
+      $this->emitOne($result, $instance->member);
+      $result->out->write('}');
+    }
+  }
+
   protected function emitUnpack($result, $unpack) {
     $result->out->write('...');
     $this->emitOne($result, $unpack->expression);
