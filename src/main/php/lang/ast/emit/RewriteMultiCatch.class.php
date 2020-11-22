4e720ea07350ee89eb4cdce52660e301bdfaa60f
Timm Friebe <thekid@thekid.de>
2020-11-22 12:11:30 +0100

    Fix PHP 7.0 compatibility for non-capturing catches

--- src/main/php/lang/ast/emit/RewriteMultiCatch.class.php
+++ src/main/php/lang/ast/emit/RewriteMultiCatch.class.php
@@ -8,15 +8,16 @@
 trait RewriteMultiCatch {
  
   protected function emitCatch($result, $catch) {
+    $capture= $catch->variable ? '$'.$catch->variable : $result->temp();
     if (empty($catch->types)) {
-      $result->out->write('catch(\\Throwable $'.$catch->variable.') {');
+      $result->out->write('catch(\\Throwable '.$capture.') {');
     } else {
       $last= array_pop($catch->types);
       $label= sprintf('c%u', crc32($last));
       foreach ($catch->types as $type) {
-        $result->out->write('catch('.$type.' $'.$catch->variable.') { goto '.$label.'; }');
+        $result->out->write('catch('.$type.' '.$capture.') { goto '.$label.'; }');
       }
-      $result->out->write('catch('.$last.' $'.$catch->variable.') { '.$label.':');
+      $result->out->write('catch('.$last.' '.$capture.') { '.$label.':');
     }
 
     $this->emitAll($result, $catch->body);
