ee056bed48d1ce36fa3469cbac344008aa619e8f
Timm Friebe <thekid@thekid.de>
2021-06-07 18:27:11 +0200

    Fix "Implicit conversion from non-compatible float" warnings
    See #112, occurs in PHP 8.1 only

--- src/test/php/lang/ast/unittest/emit/DeclareTest.class.php
+++ src/test/php/lang/ast/unittest/emit/DeclareTest.class.php
@@ -9,7 +9,7 @@ class DeclareTest extends EmittingTest {
   public function no_strict_types() {
     Assert::equals(1, $this->run('class <T> {
       public static function number(int $n) { return $n; }
-      public function run() { return self::number(1.5); }
+      public function run() { return self::number("1"); }
     }'));
   }
 
@@ -17,15 +17,15 @@ class DeclareTest extends EmittingTest {
   public function strict_types_off() {
     Assert::equals(1, $this->run('declare(strict_types = 0); class <T> {
       public static function number(int $n) { return $n; }
-      public function run() { return self::number(1.5); }
+      public function run() { return self::number("1"); }
     }'));
   }
 
-  #[Test, Expect(class: Error::class, withMessage: '/must be of (the )?type int(eger)?, float given/')]
+  #[Test, Expect(class: Error::class, withMessage: '/must be of (the )?type int(eger)?, string given/')]
   public function strict_types_on() {
     $this->run('declare(strict_types = 1); class <T> {
       public static function number(int $n) { return $n; }
-      public function run() { return self::number(1.5); }
+      public function run() { return self::number("1"); }
     }');
   }
 }
