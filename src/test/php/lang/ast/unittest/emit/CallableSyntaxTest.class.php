a9bc033dd8ce9a8e3dabe8b40fe605b3e1a2cdee
Timm Friebe <thekid@thekid.de>
2021-07-03 11:07:13 +0200

    Extract assertion into helper method

--- src/test/php/lang/ast/unittest/emit/CallableSyntaxTest.class.php
+++ src/test/php/lang/ast/unittest/emit/CallableSyntaxTest.class.php
@@ -2,100 +2,100 @@
 
 use unittest\{Assert, Test, Values};
 
+/**
+ * Tests for first-class callable syntax
+ *
+ * @see   https://wiki.php.net/rfc/first_class_callable_syntax#proposal
+ */
 class CallableSyntaxTest extends EmittingTest {
+
+  /**
+   * Verification helper
+   *
+   * @param  string $code
+   * @return void
+   * @throws unittest.AssertionFailedError
+   */
+  private function verify($code) {
+    Assert::equals(4, $this->run($code)('Test'));
+  }
 
   #[Test]
   public function native_function() {
-    $f= $this->run('class <T> {
+    $this->verify('class <T> {
       public function run() { return strlen(...); }
     }');
-
-    Assert::equals(4, $f('Test'));
   }
 
   #[Test]
   public function instance_method() {
-    $f= $this->run('class <T> {
+    $this->verify('class <T> {
       public function length($arg) { return strlen($arg); }
       public function run() { return $this->length(...); }
     }');
-
-    Assert::equals(4, $f('Test'));
   }
 
   #[Test]
   public function class_method() {
-    $f= $this->run('class <T> {
+    $this->verify('class <T> {
       public static function length($arg) { return strlen($arg); }
       public function run() { return self::length(...); }
     }');
-
-    Assert::equals(4, $f('Test'));
   }
 
   #[Test]
   public function private_method() {
-    $f= $this->run('class <T> {
+    $this->verify('class <T> {
       private function length($arg) { return strlen($arg); }
       public function run() { return $this->length(...); }
     }');
-
-    Assert::equals(4, $f('Test'));
   }
 
   #[Test]
   public function variable_function() {
-    $f= $this->run('class <T> {
+    $this->verify('class <T> {
       public function run() {
         $func= "strlen";
         return $func(...);
       }
     }');
-
-    Assert::equals(4, $f('Test'));
   }
 
   #[Test]
   public function instance_method_reference() {
-    $f= $this->run('class <T> {
+    $this->verify('class <T> {
       private $func= "strlen";
       public function run() {
         return ($this->func)(...);
       }
     }');
-
-    Assert::equals(4, $f('Test'));
   }
 
   #[Test, Values(['$this->$func(...)', '$this->{$func}(...)'])]
   public function variable_instance_method($expr) {
-    $f= $this->run('class <T> {
+    $this->verify('class <T> {
       private function length($arg) { return strlen($arg); }
       public function run() {
         $func= "length";
         return '.$expr.';
       }
     }');
-
-    Assert::equals(4, $f('Test'));
   }
 
   #[Test, Values(['self::$func(...)', 'self::{$func}(...)'])]
   public function variable_class_method($expr) {
-    $f= $this->run('class <T> {
+    $this->verify('class <T> {
       private static function length($arg) { return strlen($arg); }
       public function run() {
         $func= "length";
         return '.$expr.';
       }
     }');
-
-    Assert::equals(4, $f('Test'));
   }
 
   #[Test]
   public function variable_class_method_with_variable_class() {
-    $f= $this->run('class <T> {
+    $this->verify('class <T> {
       private static function length($arg) { return strlen($arg); }
       public function run() {
         $func= "length";
@@ -103,36 +103,28 @@ class CallableSyntaxTest extends EmittingTest {
         return $class::$func(...);
       }
     }');
-
-    Assert::equals(4, $f('Test'));
   }
 
   #[Test]
   public function string_function_reference() {
-    $f= $this->run('class <T> {
+    $this->verify('class <T> {
       public function run() { return "strlen"(...); }
     }');
-
-    Assert::equals(4, $f('Test'));
   }
 
   #[Test]
   public function array_instance_method_reference() {
-    $f= $this->run('class <T> {
+    $this->verify('class <T> {
       public function length($arg) { return strlen($arg); }
       public function run() { return [$this, "length"](...); }
     }');
-
-    Assert::equals(4, $f('Test'));
   }
 
   #[Test]
   public function array_class_method_reference() {
-    $f= $this->run('class <T> {
+    $this->verify('class <T> {
       public static function length($arg) { return strlen($arg); }
       public function run() { return [self::class, "length"](...); }
     }');
-
-    Assert::equals(4, $f('Test'));
   }
 }
