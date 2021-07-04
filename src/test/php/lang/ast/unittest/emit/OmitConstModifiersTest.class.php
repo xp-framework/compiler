c147459bbc22fb06d369de68e62e9412feb2d29b
Timm Friebe <thekid@thekid.de>
2021-07-04 21:57:14 +0200

    Add test for Omit* traits

---
+++ src/test/php/lang/ast/unittest/emit/OmitConstModifiersTest.class.php
@@ -0,0 +1,32 @@
+<?php namespace lang\ast\unittest\emit;
+
+use lang\ast\emit\{PHP, OmitConstModifiers};
+use lang\ast\nodes\{Constant, Literal};
+use lang\ast\types\IsLiteral;
+use unittest\{Assert, Test};
+
+class OmitConstModifiersTest extends EmitterTraitTest {
+
+  /** @return lang.ast.Emitter */
+  protected function fixture() {
+    return new class() extends PHP {
+      use OmitConstModifiers;
+    };
+  }
+
+  #[Test]
+  public function omits_type() {
+    Assert::equals(
+      'const TEST="test";',
+      $this->emit(new Constant([], 'TEST', new IsLiteral('string'), new Literal('"test"')))
+    );
+  }
+
+  #[Test]
+  public function omits_modifier() {
+    Assert::equals(
+      'const TEST="test";',
+      $this->emit(new Constant(['private'], 'TEST', new IsLiteral('string'), new Literal('"test"')))
+    );
+  }
+}
---
+++ src/test/php/lang/ast/unittest/emit/OmitTypesTest.class.php
@@ -0,0 +1,19 @@
+<?php namespace lang\ast\unittest\emit;
+
+use lang\ast\emit\{OmitPropertyTypes, OmitReturnTypes};
+use lang\ast\types\IsLiteral;
+use unittest\{Assert, Test};
+
+class OmitTypesTest {
+  use OmitPropertyTypes, OmitReturnTypes;
+
+  #[Test]
+  public function property_type() {
+    Assert::equals('', $this->propertyType(new IsLiteral('int')));
+  }
+
+  #[Test]
+  public function return_type() {
+    Assert::equals('', $this->returnType(new IsLiteral('int')));
+  }
+}
