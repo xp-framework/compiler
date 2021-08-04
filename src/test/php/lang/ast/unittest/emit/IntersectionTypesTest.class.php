a4f6d33a6e83309286a177235a41d478bdad69fd
Timm Friebe <thekid@thekid.de>
2021-08-04 11:50:32 +0200

    Verify field types with PHP 8.1

--- src/test/php/lang/ast/unittest/emit/IntersectionTypesTest.class.php
+++ src/test/php/lang/ast/unittest/emit/IntersectionTypesTest.class.php
@@ -48,6 +48,18 @@ class IntersectionTypesTest extends EmittingTest {
   }
 
   #[Test, Action(eval: 'new RuntimeVersion(">=8.1.0-dev")')]
+  public function field_type_restriction_with_php81() {
+    $t= $this->type('class <T> {
+      private Traversable&Countable $test;
+    }');
+
+    Assert::equals(
+      new TypeIntersection([new XPClass('Traversable'), new XPClass('Countable')]),
+      $t->getField('test')->getTypeRestriction()
+    );
+  }
+
+  #[Test, Action(eval: 'new RuntimeVersion(">=8.1.0-dev")')]
   public function parameter_type_restriction_with_php81() {
     $t= $this->type('class <T> {
       public function test(Traversable&Countable $arg) { }
@@ -56,18 +68,6 @@ class IntersectionTypesTest extends EmittingTest {
     Assert::equals(
       new TypeIntersection([new XPClass('Traversable'), new XPClass('Countable')]),
       $t->getMethod('test')->getParameter(0)->getTypeRestriction()
-    );
-  }
-
-  #[Test, Action(eval: 'new RuntimeVersion(">=8.1.0-dev")')]
-  public function parameter_type_restriction_with_php81() {
-    $t= $this->type('class <T> {
-      public function test(): Traversable&Countable { }
-    }');
-
-    Assert::equals(
-      new TypeIntersection([new XPClass('Traversable'), new XPClass('Countable')]),
-      $t->getMethod('test')->getReturnTypeRestriction()
     );
   }
 
