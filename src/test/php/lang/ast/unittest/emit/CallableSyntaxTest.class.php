95c170b9326d113ac08d922991c2b57f983aa536
Timm Friebe <thekid@thekid.de>
2021-10-05 22:21:02 +0200

    Test new T(...) inside array_map()

--- src/test/php/lang/ast/unittest/emit/CallableSyntaxTest.class.php
+++ src/test/php/lang/ast/unittest/emit/CallableSyntaxTest.class.php
@@ -161,6 +161,16 @@ class CallableSyntaxTest extends EmittingTest {
   }
 
   #[Test]
+  public function instantiation_in_map() {
+    $r= $this->run('use lang\ast\unittest\emit\Handle; class <T> {
+      public function run() {
+        return array_map(new Handle(...), [0, 1, 2]);
+      }
+    }');
+    Assert::equals([new Handle(0), new Handle(1), new Handle(2)], $r);
+  }
+
+  #[Test]
   public function anonymous_instantiation() {
     $f= $this->run('class <T> {
       public function run() {
