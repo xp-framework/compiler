4f1e147bbbd23b1ba136b067524f590fb622e984
Timm Friebe <thekid@thekid.de>
2022-01-05 21:07:38 +0100

    Add tests for all lang.ast.emit.Escaping methods

---
+++ src/test/php/lang/ast/unittest/emit/EscapingTest.class.php
@@ -0,0 +1,54 @@
+<?php namespace lang\ast\unittest\emit;
+
+use io\streams\MemoryOutputStream;
+use lang\ast\emit\Escaping;
+use unittest\{Assert, Test};
+
+class EscapingTest {
+
+  #[Test]
+  public function can_create() {
+    new Escaping(new MemoryOutputStream(), []);
+  }
+
+  #[Test]
+  public function escape_single_quote() {
+    $out= new MemoryOutputStream();
+    $fixture= new Escaping($out, ["'" => "\\'"]);
+    $fixture->write("'Hello', he said");
+
+    Assert::equals("\\'Hello\\', he said", $out->bytes());
+  }
+
+  #[Test]
+  public function calls_underlying_flush() {
+    $out= new class() extends MemoryOutputStream {
+      public $flushed= false;
+
+      public function flush() {
+        $this->flushed= true;
+        parent::flush();
+      }
+    };
+    $fixture= new Escaping($out, []);
+    $fixture->flush();
+
+    Assert::true($out->flushed);
+  }
+
+  #[Test]
+  public function calls_underlying_close() {
+    $out= new class() extends MemoryOutputStream {
+      public $closed= false;
+
+      public function close() {
+        $this->closed= true;
+        parent::close();
+      }
+    };
+    $fixture= new Escaping($out, []);
+    $fixture->close();
+
+    Assert::true($out->closed);
+  }
+}
