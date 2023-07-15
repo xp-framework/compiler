514de5d3409188d498d7ea2c5e28752d0d94922e
Timm Friebe <thekid@thekid.de>
2023-07-15 17:38:19 +0200

    Run code in new runtime for PHP 8.3

--- src/test/php/lang/ast/unittest/emit/MethodOverridingTest.class.php
+++ src/test/php/lang/ast/unittest/emit/MethodOverridingTest.class.php
@@ -1,16 +1,42 @@
 <?php namespace lang\ast\unittest\emit;
 
+use lang\Runtime;
 use lang\ast\Error;
 use test\{Assert, Test};
 
 class MethodOverridingTest extends EmittingTest {
 
+  /**
+   * Returns emitters to use. Defaults to XpMeta
+   *
+   * @return string[]
+   */
+  protected function emitters() { return []; }
+
+  /**
+   * Verifies code, yielding any errors
+   *
+   * @param  string $code
+   * @return ?string
+   */
   private function verify($code) {
+
+    // PHP >= 8.3: PHP will raise errors for us
+    if (PHP_VERSION_ID >= 80300) {
+      $rt= Runtime::getInstance()->newInstance(null, 'class', 'xp.runtime.Evaluate', [str_replace('%T', 'T', $code)]);
+      $rt->in->close();
+      $err= $rt->err->readLine();
+      $rt->close();
+
+      return false === $err ? null : preg_replace('/Uncaught error: Compile error \((.+)\)/', '$1', $err);
+    }
+
+    // PHP < 8.3: XP compiler will raise errors for us
     try {
-      $this->declare($code);
+      $t= $this->declare($code);
       return null;
     } catch (Error $e) {
-      return preg_replace('/T[0-9]+/', '%T', $e->getMessage());
+      return preg_replace('/T[0-9]+/', 'T', $e->getMessage());
     }
   }
 
@@ -38,7 +64,7 @@ class MethodOverridingTest extends EmittingTest {
   #[Test]
   public function without_parent() {
     Assert::equals(
-      '%T::fixture() has #[\Override] attribute, but no matching parent method exists',
+      'T::fixture() has #[\Override] attribute, but no matching parent method exists',
       $this->verify('class %T {
         #[Override]
         public function fixture() { }
@@ -49,7 +75,7 @@ class MethodOverridingTest extends EmittingTest {
   #[Test]
   public function overriding_non_existant_method() {
     Assert::equals(
-      '%T::nonExistant() has #[\Override] attribute, but no matching parent method exists',
+      'T::nonExistant() has #[\Override] attribute, but no matching parent method exists',
       $this->verify('class %T extends \lang\Throwable {
         #[Override]
         public function nonExistant() { }
