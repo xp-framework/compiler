8fc2c0af40bc14c4ae46f67cd04e9071164f10e0
Timm Friebe <thekid@thekid.de>
2025-04-21 14:32:53 +0200

    Add test for clone operator

---
+++ src/test/php/lang/ast/unittest/emit/CloningTest.class.php
@@ -0,0 +1,57 @@
+<?php namespace lang\ast\unittest\emit;
+
+use test\{Assert, Before, Test};
+
+/** @see https://www.php.net/manual/en/language.oop5.cloning.php */
+class CloningTest extends EmittingTest {
+  private $fixture;
+
+  #[Before]
+  public function fixture() {
+    $this->fixture= new class() {
+      public $id= 1;
+
+      public function with($id) {
+        $this->id= $id;
+        return $this;
+      }
+
+      public function __clone() {
+        $this->id++;
+      }
+    };
+  }
+
+  #[Test]
+  public function clone_operator() {
+    $clone= $this->run('class %T {
+      public function run($in) {
+        return clone $in;
+      }
+    }', $this->fixture);
+
+    Assert::true($clone instanceof $this->fixture && $this->fixture !== $clone);
+  }
+
+  #[Test]
+  public function clone_function() {
+    $clone= $this->run('class %T {
+      public function run($in) {
+        return clone($in);
+      }
+    }', $this->fixture);
+
+    Assert::true($clone instanceof $this->fixture && $this->fixture !== $clone);
+  }
+
+  #[Test]
+  public function clone_interceptor_called() {
+    $clone= $this->run('class %T {
+      public function run($in) {
+        return clone $in;
+      }
+    }', $this->fixture->with(id: 1));
+
+    Assert::equals([1, 2], [$this->fixture->id, $clone->id]);
+  }
+}
