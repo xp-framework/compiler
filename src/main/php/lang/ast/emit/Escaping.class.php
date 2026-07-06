0cbc4666326de2aeab56c0c2e3614b89c2dbf9d5
Timm Friebe <thekid@thekid.de>
2026-07-06 23:10:27 +0200

    Use constructor argument promotion (more / partial cases)

--- src/main/php/lang/ast/emit/Escaping.class.php
+++ src/main/php/lang/ast/emit/Escaping.class.php
@@ -3,12 +3,8 @@
 use io\streams\OutputStream;
 
 class Escaping implements OutputStream {
-  private $target, $replacements;
 
-  public function __construct(OutputStream $target, array $replacements) {
-    $this->target= $target;
-    $this->replacements= $replacements;
-  }
+  public function __construct(private OutputStream $target, private array $replacements) { }
 
   public function write($bytes) {
     $this->target->write(strtr($bytes, $this->replacements));
