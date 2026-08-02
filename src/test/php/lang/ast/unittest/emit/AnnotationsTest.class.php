c44d821c8d90cb14268ad28e8b25a46117d45e0a
Timm Friebe <thekid@thekid.de>
2026-08-02 11:45:16 +0200

    Emit PHP 8 attributes alongside XP meta information

--- src/test/php/lang/ast/unittest/emit/AnnotationsTest.class.php
+++ src/test/php/lang/ast/unittest/emit/AnnotationsTest.class.php
@@ -1,6 +1,8 @@
 <?php namespace lang\ast\unittest\emit;
 
+use ReflectionClass;
 use lang\ast\emit\php\XpMeta;
+use test\{Assert, Test, Values};
 
 /**
  * Annotations via XP Meta information
@@ -13,4 +15,22 @@ class AnnotationsTest extends AnnotationSupport {
   /** @return string[] */
   protected function emitters() { return [XpMeta::class]; }
 
+  /** @return iterable */
+  private function declarations() {
+    yield ['#[Test]', ['Test' => []]];
+    yield ['#[Test("a")]', ['Test' => ['a']]];
+    yield ['#[Test(1, 2, 3)]', ['Test' => [1, 2, 3]]];
+    yield ['#[Test(value: "a")]', ['Test' => ['value' => 'a']]];
+  }
+
+  #[Test, Values(from: 'declarations')]
+  public function also_emits_php_attributes($declaration, $expected) {
+    $type= $this->declare($declaration);
+    $declared= [];
+    foreach ((new ReflectionClass($type->literal()))->getAttributes() as $attribute) {
+      $declared[$attribute->name]= $attribute->getArguments();
+    }
+
+    Assert::equals($expected, $declared);
+  }
 }
